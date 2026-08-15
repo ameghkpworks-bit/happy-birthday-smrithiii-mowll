cf435fd6d459d78b44852fd0a94e2d2716776067
ameghkpworks-bit <ameghkpworks@gmail.com>
2026-08-15 23:39:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    base: "/happy-birthday-smrithiii-mowll/",
+  },
+
   tanstackStart: {
+    server: { entry: "server" },
+
     spa: {
       enabled: true,
-      prerender: {
-        outputPath: "/index.html",
-      },
     },
   },
 });
